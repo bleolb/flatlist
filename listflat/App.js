8b4a9a6f48b6140686d4231ebf42649e674b5f1c
unknown <ble.ona@yavirac.edu.ec>
2020-02-03 22:36:34 -0500

    flatlist

---
+++ listflat/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  FlatList,
+} from 'react-native';
+
+export default class EventList extends Component {
+  state = {
+    libros: []
+  }
+
+  componentDidMount() {
+    const libros = require('./libros.json').libros
+    this.setState( { libros })
+  }
+
+  render() {
+    return(
+      <FlatList
+        data={ this.state.libros }
+        style={ styles.list }
+        renderItem={ ({ item }) => 
+          <View style={styles.container}>
+            <Text>{ item.nombre } Autor=> {item.autor} </Text>
+          </View>
+        }
+        keyExtractor={ item => item.id }
+      />
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  list: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: '#F3F3F3'
+  },
+});
